277d3c4575607e0282a3754d5095b19d9f9298ff
MattEstAuSommet <59025806+MattEstAuSommet@users.noreply.github.com>
2020-09-08 16:48:14 +0200

    Second version..

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 document.getElementById("pause").onclick = () => {
 	if (PAUSE) {
 		PAUSE = false;
@@ -29,7 +30,7 @@ document.getElementById("scale").onchange = () => {
 	SCALE = document.getElementById("scale").value;
 }
 
-var ox = 0; var oy = 0; var or = 40;
+var ox = 0; var oy = 0; var or = 10;
 document.getElementById("obstacle").onclick = () => {
 	if (OBSTACLES.length > 0) {
 		document.getElementById("obstacle").innerHTML = "put obstacle";
@@ -64,3 +65,61 @@ document.getElementById("or").onchange = () => {
 
 setup(300);
 run();
+*/
+
+const canvas = document.getElementById("main-canvas");
+var left_down = false; var right_down = false;
+
+BOIDS.options.background_color = "#fba6ff";
+BOIDS.options.boid_color = "white";
+BOIDS.options.obstacle_color = "white";
+
+canvas.addEventListener('contextmenu', event => event.preventDefault());
+
+canvas.addEventListener("mousedown", (evt) => { 
+	if (evt.button == 0) {
+		left_down = true;
+		right_down = false;
+	} else if (evt.button == 2) {
+		right_down = true;
+		left_down = false;
+		
+	}
+});
+
+document.addEventListener("mouseup", (evt) => { 
+	if (evt.button == 0) {
+		left_down = false;
+	} else {
+		right_down = false;
+	}
+});
+
+canvas.addEventListener("mousemove", (evt) => {
+	if (left_down) {
+		BOIDS.options.cam_x -= evt.movementX/BOIDS.options.scale;
+		BOIDS.options.cam_y -= evt.movementY/BOIDS.options.scale;
+
+		BOIDS.legalize_cam();
+	} else if (right_down) {
+		let position = BOIDS.cam_to_real(evt.offsetX, evt.offsetY);
+		BOIDS.obstacles.push(new BOIDS.Obstacle(position.x, position.y, 3));
+	}
+});
+
+canvas.addEventListener("wheel", (evt) => {
+	if (evt.deltaY > 0 && BOIDS.options.scale > 1) {
+		BOIDS.options.scale -= 0.1;
+		BOIDS.legalize_cam();
+	} else if (evt.deltaY < 0) {
+		let direction = BOIDS.cam_to_real(evt.offsetX, evt.offsetY).sub(new BOIDS.Vector(BOIDS.options.cam_x, BOIDS.options.cam_y));
+		BOIDS.options.cam_x += direction.x/BOIDS.options.scale**4;
+		BOIDS.options.cam_y += direction.y/BOIDS.options.scale**4;
+		BOIDS.options.scale += 0.1;
+		BOIDS.legalize_cam();
+	}
+});
+
+BOIDS.set_canvas(canvas);
+BOIDS.setup(300);
+BOIDS.run();
